b51d5dd983ffd71366eb8f4cfc55f2a3ccb02a0a
Antonio Selvatici <antoniohps@gmail.com>
2019-07-25 09:45:33 -0300

    Peeking input messages from server

--- contentScript.js
+++ contentScript.js
@@ -18,7 +18,6 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-let __TCUDPSocketId = -1;
 let __TCPostPort = 0; 
 let __TCmonitorState = false;
 let __TCprevLen = 0;
@@ -43,7 +42,7 @@ const __TCobserver = new MutationObserver(
     });
 
 // Envia mensagens para o popup script e para
-// a porta UDP
+// o servidor
 function sendMessage(msg) {
 
     if (__TCPostPort > 0) {
@@ -59,8 +58,31 @@ function sendMessage(msg) {
             origin: 'tinkermonitor',
             payload: msg
         }, function (response) { });
-
     }
+}
+
+// Consulta novas mensagens a swere enviadas ao Arduino
+function queryInput(port, inputPanel) {
+    let ajax = new XMLHttpRequest();
+    // Seta tipo de requisição e URL com os parâmetros
+    ajax.open("GET", "http://localhost:"+port+"/tinkermon", true);
+    // Envia a requisição
+    ajax.send();
+    // Cria um evento para receber o retorno.
+    ajax.onreadystatechange = function () {
+        // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+        if (ajax.readyState == 4 && [200, 304].indexOf(ajax.status) >= 0 ) {
+            inputPanel.value = ajax.responseText.trim();
+            //Cria e dispara o evento de keyboard (enter)
+            let event = new KeyboardEvent('keypress', {keyCode: 13, which: 13, key: 13 });
+            inputPanel.dispatchEvent(event);
+
+            // Agenda uma nova chamada
+            if (__TCmonitorState)
+                setTimeout(function () { queryInput(port, inputPanel); }, 2000);
+        }
+    }
+
 }
 
 // Inicia o monitoramento do Serial Monitor
@@ -70,11 +92,14 @@ function startMonitor(port) {
 
     __TCmonitorState = false;
 
-    const selector = "body > div.root_container.js-editor.js-editor--circuit.editorContainer > div.editor.js-root_container > div.editor__content.js-editor__content > div.js-tpl-target__code_panel > div > div.code_panel__serial.js-code_panel__serial > div.code_panel__serial__content.js-code_panel__serial__content > div.code_panel__serial__top > div.code_panel__serial__content__text.js-code_panel__serial__text.js-code_editor__serial-monitor__content";
-    const textPanel = document.querySelector(selector);
-    if (textPanel) {
+    const selectorText = "body > div.root_container.js-editor.js-editor--circuit.editorContainer > div.editor.js-root_container > div.editor__content.js-editor__content > div.js-tpl-target__code_panel > div > div.code_panel__serial.js-code_panel__serial > div.code_panel__serial__content.js-code_panel__serial__content > div.code_panel__serial__top > div.code_panel__serial__content__text.js-code_panel__serial__text.js-code_editor__serial-monitor__content";
+    const selectorInput = "body > div.root_container.js-editor.js-editor--circuit.editorContainer > div.editor.js-root_container > div.editor__content.js-editor__content > div.js-tpl-target__code_panel > div > div.code_panel__serial.js-code_panel__serial > div.code_panel__serial__content.js-code_panel__serial__content > div.code_panel__serial__bottom.js-code_panel__serial__bottom.js-code_editor__serial-monitor__bottom.clearfix > input";
+    const textPanel = document.querySelector(selectorText);
+    const inputPanel= document.querySelector(selectorInput);
+    if (textPanel && inputPanel) {
         __TCPostPort = Math.abs(parseInt('' + port)); //Enforce port is positive integer
         __TCobserver.observe(textPanel, { attributes: true, childList: true, attributeOldValue: true });
+        queryInput(__TCPostPort, inputPanel);
         __TCmonitorState = true;
     }
 }
